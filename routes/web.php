c450d7a9973b2424547023971654da12023382b0
Irine Egorenkova <bon-travel@yandex.ru>
2018-12-05 18:06:57 +0300

    Update file

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,23 @@
 <?php
-
-Route::get('/', function () {
-    return redirect()->route('question.index');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::resource('question', 'QuestionController', ['only'=>[
-    'index', 'create', 'store'
-]]);
-
-Route::group(['middleware' => 'auth'], function () {
-    Route::resource('user', 'UserController', ['except'=>[
-        'show'
-    ]]);
-
-    Route::resource('topic', 'TopicController', ['except'=>[
-        'edit', 'update'    
-    ]]);
-    
-    Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-        Route::resource('question', 'Admin\QuestionController', ['only'=>[
-            'edit', 'update', 'destroy'
-        ]]);
+    Route::get('/', function () {
+        return redirect()->route('question.index');
     });
-});
+    Auth::routes();
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('question', 'QuestionController', ['only'=>[
+        'index', 'create', 'store'
+    ]]);
+    Route::group(['middleware' => 'auth'], function () {
+        Route::resource('user', 'UserController', ['except'=>[
+            'show'
+        ]]);
+        Route::resource('topic', 'Admin\TopicController', ['except'=>[
+            'edit', 'update'
+        ]]);
+
+        Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+            Route::resource('question', 'Admin\QuestionController', ['only'=>[
+                'edit', 'update', 'destroy'
+            ]]);
+        });
+    });
